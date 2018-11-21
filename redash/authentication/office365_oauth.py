9c5c9c13e221aa3a9ab27eea4e8731c669912c46
Chris Zubak-Skees <chriszs@gmail.com>
2018-11-21 15:07:58 -0500

    Fix the Liz Essley Whyte problem

--- redash/authentication/office365_oauth.py
+++ redash/authentication/office365_oauth.py
@@ -62,7 +62,7 @@ def create_and_login_user(org, name, email):
         if user_object.name != name:
             logger.debug("Updating user name (%r -> %r)", user_object.name, name)
             user_object.name = name
-            user_object.commit()
+            models.db.session.commit()
     except NoResultFound:
         logger.debug("Creating user object (%r)", name)
         user_object = models.User(org=org, name=name, email=email, group_ids=[org.default_group.id])
